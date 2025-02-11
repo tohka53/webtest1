c6b3c8d2b782e59a0d75997301d4bb71ee5b96f0
Miguel E. Cabrera <mecg1994@gmail.com>
2025-02-10 22:56:10 -0600

    cambios

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Formulario de Perfil de Cliente - Qualitech</title>
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+        <script src="https://html2canvas.hertzen.com/dist/html2canvas.min.js"></script>
+      </Head>
+
+      <div className="bg-gray-100 p-6">
+        {
+        
+        
+        
+        
+        
+        
+        }
+      </div>
+    </>
+  );
+}
